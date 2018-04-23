31b0f2f36f3c0b54c979a9d16b8eb929d434c015
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/SilverlightApplication86/MainPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace SilverlightApplication86
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            richeditor.ApplyTemplate();
+            richeditor.Loaded += new RoutedEventHandler(richeditor_Loaded);
+        }
+
+        void richeditor_Loaded(object sender, RoutedEventArgs e)
+        {
+            richeditor.RichControl.Text = @"Use control template to selectively hide AgRichEdit elements.
+In this example the Zoom slider in the status bar is hidden.";
+            
+        }
+    }
+}
---
+++ VB/SilverlightApplication86/MainPage.xaml.vb
@@ -0,0 +1,19 @@
+﻿Imports Microsoft.VisualBasic
+Imports System.Windows
+Imports System.Windows.Controls
+
+Namespace SilverlightApplication86
+	Partial Public Class MainPage
+		Inherits UserControl
+		Public Sub New()
+			InitializeComponent()
+			richeditor.ApplyTemplate()
+			AddHandler richeditor.Loaded, AddressOf richeditor_Loaded
+		End Sub
+
+		Private Sub richeditor_Loaded(ByVal sender As Object, ByVal e As RoutedEventArgs)
+			richeditor.RichControl.Text = "Use control template to selectively hide AgRichEdit elements." & ControlChars.CrLf & "In this example the Zoom slider in the status bar is hidden."
+
+		End Sub
+	End Class
+End Namespace
